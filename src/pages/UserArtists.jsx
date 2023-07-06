b004c12f02cf71783c1460be97c806a0401040b8
Gabriel Ferreira <96392170+GabrielFerreiraAlves@users.noreply.github.com>
2023-07-06 11:24:09 -0300

    changes

--- src/pages/UserArtists.jsx
+++ src/pages/UserArtists.jsx
@@ -1,6 +1,9 @@
 import {useEffect,useState} from 'react'
+import {Link} from 'react-router-dom'
 
 const UserArtists = () => {
+  const [artists,setArtists] = useState([])
+
   const url = 'https://api.spotify.com/v1/me/following?type=artist' 
   const token = localStorage.getItem('accessToken')
   const headers = {
@@ -12,11 +15,21 @@ const UserArtists = () => {
   useEffect(() => {
     fetch(url,headers)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data)
+      setArtists(data.artists.items)
+    })
   },[])
-
+// artists.name artists.id artists.images[1].url
   return (
-    <div>UserArtists</div>
+    <div>
+      {artists && artists.map(el => (
+        <Link to={`/artist/${el.id}`}>
+          <img src={el.images[1].url} alt="" />
+          <p>{el.name}</p>
+        </Link>
+      ))}
+    </div>
   )
 }
 
